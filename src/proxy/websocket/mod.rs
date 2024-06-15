25cb5d5a7231fd091e85d96304bd4691c3304579
ssrlive <30760636+ssrlive@users.noreply.github.com>
2024-06-15 17:34:51 +0800

    fix webstream do not flush data

--- src/proxy/websocket/mod.rs
+++ src/proxy/websocket/mod.rs
@@ -91,6 +91,9 @@ impl<T: ProxySteam> AsyncWrite for BinaryWsStream<T> {
         Pin::new(&mut self.inner)
             .start_send(message)
             .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("{:?}", e)))?;
+       let _p =  Pin::new(&mut self.inner)
+            .poll_flush(cx)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("{:?}", e)))?;
         Poll::Ready(Ok(buf.len()))
     }
 
